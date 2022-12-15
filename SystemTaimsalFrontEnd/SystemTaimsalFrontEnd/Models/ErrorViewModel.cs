c6a9b6ef9a252598b2d7a35ede292095b7bd7bb3
cesardavidhq2002 <cesardavidHQ-2002@outlook.com>
2022-12-14 23:09:30 -0600

    Nuevo Repositorio

---
+++ SystemTaimsalFrontEnd/SystemTaimsalFrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SystemTaimsalFrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
