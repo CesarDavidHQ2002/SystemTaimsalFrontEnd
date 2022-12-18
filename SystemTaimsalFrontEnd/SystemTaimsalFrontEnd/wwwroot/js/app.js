7b96db5e0e8ae77fac0e916d316bfa0e266b3929
cesardavidhq2002 <cesardavidHQ-2002@outlook.com>
2022-12-18 02:36:41 -0600

    footer modificado, proveedores y clientes agregados en carrusel

--- SystemTaimsalFrontEnd/SystemTaimsalFrontEnd/wwwroot/js/app.js
+++ SystemTaimsalFrontEnd/SystemTaimsalFrontEnd/wwwroot/js/app.js
@@ -26,4 +26,31 @@ window.addEventListener('load', function(){
 			}
 		]
 	});
+	new Glider(document.querySelector('.carousel_lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel_indicadores',
+		arrows: {
+			prev: '.carousel_anterior',
+			next: '.carousel_siguiente'
+		},
+		responsive: [
+			{
+				// screens greater than >= 775px
+				breakpoint: 450,
+				settings: {
+					// Set to `auto` and provide item width to adjust to viewport
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			}, {
+				// screens greater than >= 1024px
+				breakpoint: 800,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4
+				}
+			}
+		]
+	});
 });
